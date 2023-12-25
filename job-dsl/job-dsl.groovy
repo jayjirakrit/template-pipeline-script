7af4a4330cb3f234b21368458ebf3250dec25cc4
Jirakrit <jayjirakrit@gmail.com>
2023-12-26 04:44:30 +0700

    fix syntax

--- job-dsl/job-dsl.groovy
+++ job-dsl/job-dsl.groovy
@@ -1,9 +1,9 @@
-import groovy.yaml.YamlSlurper
+@Grab('org.yaml:snakeyaml:1.17')
+import org.yaml.snakeyaml.Yaml
 
 def jobName = "sample-job-test"
 def rawPipeline = readFileFromWorkspace("${pipelineFile}")
-def config = new YamlSlurper().parseText(rawPipeline)
-
+def config = new Yaml().load(("${pipelineFile}" as File).text)
 job(jobName) {
     scm {
         github('jenkinsci/job-dsl-plugin', 'master', 'ssh')
