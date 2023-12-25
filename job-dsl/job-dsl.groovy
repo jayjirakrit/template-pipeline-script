e698abab05378330a29064e82eeb75ddc8e68699
Jirakrit <jayjirakrit@gmail.com>
2023-12-26 04:11:22 +0700

    fix syntax

--- job-dsl/job-dsl.groovy
+++ job-dsl/job-dsl.groovy
@@ -1,9 +1,12 @@
 def jobName = "sample-job-test"
+def credential = "${credentials}"
+
 job(jobName) {
     scm {
         github('jenkinsci/job-dsl-plugin', 'master', 'ssh')
     }
     steps {
         shell("echo Create Job ${jobName}")
+        shell("echo Credential ${credential}")
     }
 }
