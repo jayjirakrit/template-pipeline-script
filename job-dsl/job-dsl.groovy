ba74a0395f328d00a3dbc068c2a216d79baffbe0
Jirakrit <jayjirakrit@gmail.com>
2023-12-26 04:24:37 +0700

    fix syntax

--- job-dsl/job-dsl.groovy
+++ job-dsl/job-dsl.groovy
@@ -1,12 +1,12 @@
 def jobName = "sample-job-test"
 def credential = "${credentials}"
-
+def pipelinePath = readFileFromWorkspace("${pipelineFile}")
 job(jobName) {
     scm {
         github('jenkinsci/job-dsl-plugin', 'master', 'ssh')
     }
     steps {
-        shell("echo Create Job ${jobName}")
+        shell("echo Create Job ${pipelinePath.url}")
         shell("echo Credential ${credential}")
     }
 }
