4fed6f856f2fda785e319ad8e5340ed1e5764535
Jirakrit <jayjirakrit@gmail.com>
2023-12-26 04:37:02 +0700

    fix syntax

--- job-dsl/job-dsl.groovy
+++ job-dsl/job-dsl.groovy
@@ -1,11 +1,16 @@
+import groovy.util.ConfigSlurper
+
 def jobName = "sample-job-test"
 def credential = "${credentials}"
 def pipelinePath = readFileFromWorkspace("${pipelineFile}")
+def configSlurper = new ConfigSlurper().parse(new File(${pipelineFile}).text)
+
 job(jobName) {
     scm {
         github('jenkinsci/job-dsl-plugin', 'master', 'ssh')
     }
     steps {
         shell("echo Credential ${pipelinePath}")
+        shell("echo Credential ${configSlurper.parameters.url}")
     }
 }
